b45c57adfc88e6c39a77b5625685701a45fb46e6
blackchestnut <alexander.kalinichev@gmail.com>
2020-01-15 00:05:52 +0300

    Update config for custom domain

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:blackchestnut/mybudget2.git master:gh-pages
+git push -f git@github.com:mybudget-ws/mybudget.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
+// https://cli.vuejs.org/guide/deployment.html#github-pages
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production' ?
+//     '/mybudget2/' :
+//     '/'
+// };
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ?
-    '/mybudget2/' :
+    '/' :
     '/'
 };
