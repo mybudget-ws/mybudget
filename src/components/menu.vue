ad857fd8e7e43e521e96998298f7627bff9c344b
blackchestnut <alexander.kalinichev@gmail.com>
2020-01-27 23:14:02 +0300

    mobile menu

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,77 +1,98 @@
 <template>
-  <nav v-if='isShortMenu' class='white z-depth-0 '>
-    <div class='nav-wrapper container'>
-      <router-link
-        to='/'
-        class='brand-logo grey-text left'
-      >
-        myBudget v2.0
-      </router-link>
-      <ul id='nav-mobile' class='right'>
-        <li>
-          <router-link to='/currencies' :class='btnClasses'>
-            Курсы валют
-          </router-link>
-        </li>
-        <li>
-          <router-link to='/login' :class='btnClasses'>
-            Вход
-          </router-link>
-        </li>
-      </ul>
-    </div>
-  </nav>
-  <nav v-else class='blue-grey darken-4 z-depth-0 '>
-    <div class='nav-wrapper container'>
-      <ul id='nav-mobile'>
-        <li>
-          <router-link to='/transactions' :class='btnClasses'>
-            Операции
-          </router-link>
-        </li>
-        <li>
-          <router-link to='/accounts' :class='btnClasses'>
-            Счета
-          </router-link>
-        </li>
-        <li>
-          <router-link to='/reports' :class='btnClasses'>
-            Отчеты
-          </router-link>
-        </li>
-        <li>
-          <router-link to='/categories' :class='btnClasses'>
-            Категории
-          </router-link>
-        </li>
-        <li>
-          <router-link to='/budgets' :class='btnClasses'>
-            Бюджеты
-          </router-link>
-        </li>
-        <li>
-          <router-link to='/projects' :class='btnClasses'>
-            Проекты
-          </router-link>
-        </li>
-        <li class='right' title='Выход'>
-          <a :class='btnClasses' @click='exit'>
-            <i class='material-icons'>exit_to_app</i>
-          </a>
-        </li>
-        <li class='right' title='Настройки'>
-          <router-link to='/settings' :class='btnClasses'>
-            <i class='material-icons'>settings</i>
-          </router-link>
-        </li>
-        <li class='right' title='Курсы валют'>
-          <router-link to='/currencies' :class='btnClasses'>
-            <i class='material-icons'>euro_symbol</i>
-          </router-link>
-        </li>
-      </ul>
-    </div>
-  </nav>
+  <div>
+    <nav v-if='isShortMenu' class='white z-depth-0 '>
+      <div class='nav-wrapper container'>
+        <router-link
+          to='/'
+          class='brand-logo grey-text left'
+        >
+          myBudget v2.0
+        </router-link>
+        <ul class='right'>
+          <li>
+            <router-link to='/currencies' :class='btnClasses'>
+              Курсы валют
+            </router-link>
+          </li>
+          <li>
+            <router-link to='/login' :class='btnClasses'>
+              Вход
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <nav v-else class='blue-grey darken-4 z-depth-0 '>
+      <div class='nav-wrapper container'>
+        <a href="#" data-target='mobile-demo' class='sidenav-trigger'><i class='material-icons'>menu</i></a>
+        <ul class='hide-on-med-and-down'>
+          <li>
+            <router-link to='/transactions' :class='btnClasses'>
+              Операции
+            </router-link>
+          </li>
+          <li>
+            <router-link to='/accounts' :class='btnClasses'>
+              Счета
+            </router-link>
+          </li>
+          <li>
+            <router-link to='/reports' :class='btnClasses'>
+              Отчеты
+            </router-link>
+          </li>
+          <li>
+            <router-link to='/categories' :class='btnClasses'>
+              Категории
+            </router-link>
+          </li>
+          <li>
+            <router-link to='/budgets' :class='btnClasses'>
+              Бюджеты
+            </router-link>
+          </li>
+          <li>
+            <router-link to='/projects' :class='btnClasses'>
+              Проекты
+            </router-link>
+          </li>
+          <li class='right' title='Выход'>
+            <a :class='btnClasses' @click='exit'>
+              <i class='material-icons'>exit_to_app</i>
+            </a>
+          </li>
+          <li class='right' title='Настройки'>
+            <router-link to='/settings' :class='btnClasses'>
+              <i class='material-icons'>settings</i>
+            </router-link>
+          </li>
+          <li class='right' title='Курсы валют'>
+            <router-link to='/currencies' :class='btnClasses'>
+              <i class='material-icons'>euro_symbol</i>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <ul
+      v-if='!isShortMenu'
+      id='mobile-demo'
+      ref='sidenav'
+      class='sidenav'
+    >
+      <li><a href='/transactions'>Операции</a></li>
+      <li><a href='/accounts'>Счета</a></li>
+      <li><a href='/reports'>Отчеты</a></li>
+      <li><a href='/categories'>Категории</a></li>
+      <li><a href='/budgets'>Бюджеты</a></li>
+      <li><a href='/projects'>Проекты</a></li>
+      <li class='divider' />
+      <li><a href='/currencies'>Курсы валют</a></li>
+      <li><a href='/settings'>Настройки</a></li>
+      <li class='divider' />
+      <li><a @click='exit'>Выход</a></li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -90,11 +111,19 @@ export default {
         'waves-effect blue-grey-text text-lighten-5';
     }
   },
+  mounted() {
+    /* eslint-disable */
+    M.Sidenav.init(this.$refs.sidenav, {});
+    /* eslint-enable */
+  },
   methods: {
     logout: call('user/logout'),
     exit() {
       this.logout();
-      this.$router.replace({ name: 'home' });
+      // NOTE: Use location to reload the page and hide overlay
+      // this.$refs.sidenav.click();
+      // this.$router.replace({ name: 'home' });
+      window.location.href = '/';
     }
   }
 };
