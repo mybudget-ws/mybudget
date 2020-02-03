b3b11bdd9dd44551672255d06e0310a87d2eb82d
blackchestnut <alexander.kalinichev@gmail.com>
2020-02-04 01:25:20 +0300

    Update loader component

--- src/components/loader.vue
+++ src/components/loader.vue
@@ -1,5 +1,19 @@
 <template>
-  <p>Загрузка...</p>
+  <div class='loader-wrapper'>
+    <div class='preloader-wrapper big active'>
+      <div class='spinner-layer spinner-green-only'>
+        <div class='circle-clipper left'>
+          <div class='circle' />
+        </div>
+        <div class='gap-patch'>
+          <div class='circle' />
+        </div>
+        <div class='circle-clipper right'>
+          <div class='circle' />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,4 +27,7 @@ export default {
 </script>
 
 <style scoped lang='sass'>
+.loader-wrapper
+  text-align: center
+  padding-top: 50px
 </style>
