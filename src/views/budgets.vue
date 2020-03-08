b75e942d1e970ffee23852905f9524a7c62a85f2
blackchestnut <alexander.kalinichev@gmail.com>
2020-03-09 01:32:13 +0300

    misc

--- src/views/budgets.vue
+++ src/views/budgets.vue
@@ -2,11 +2,17 @@
   <div>
     <Menu />
     <div class='container'>
-      <PageHeader name='Бюджеты' action='/accounts/new' />
+      <!--PageHeader name='Бюджеты' action='/budgets/new' /-->
+      <PageHeader name='Бюджеты' />
       <Loader v-if='isLoading' />
       <div class='row'>
         <div class='col'>
-          TODO: Budgets
+          <div class='card blue-grey darken-1'>
+            <div class='card-content white-text'>
+              <span class='card-title'>Скоро</span>
+              <p>Данный фукнционал находится в разработке</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
