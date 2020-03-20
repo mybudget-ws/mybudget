4bf0d83392d380fbb701322dc80c521a67b308ed
blackchestnut <alexander.kalinichev@gmail.com>
2020-03-21 00:37:47 +0300

    misc

--- src/views/goals/index.vue
+++ src/views/goals/index.vue
@@ -40,7 +40,7 @@
                 <td class='percentage'>
                   <Amount :value='item.balance' :currency='currency(item)'>
                     <span
-                      class='new badge black-text tag grey lighten-2'
+                      class='new badge black-text tag grey lighten-1'
                       :data-badge-caption='tagPercentage(item)'
                     />
                   </Amount>
