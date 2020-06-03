e4d5e065f6705f214dff560fb838f153895a5c40
blackchestnut <alexander.kalinichev@gmail.com>
2020-06-04 00:28:51 +0300

    Add GA

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './app.vue';
 import router from './router';
 import store from './store/index';
 
+import VueAnalytics from 'vue-analytics';
+if (process.env.NODE_ENV === 'production') {
+  Vue.use(VueAnalytics, {
+    id: 'UA-49766114-6',
+    router
+  });
+}
+
 import 'materialize-css';
 import 'materialize-css/dist/css/materialize.css';
 
