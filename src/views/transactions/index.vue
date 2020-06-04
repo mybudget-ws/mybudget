30096aac904104dd7c8dfd6932758ad12b7ec062
blackchestnut <alexander.kalinichev@gmail.com>
2020-06-04 21:37:48 +0300

    Fix copy action for blank description

--- src/views/transactions/index.vue
+++ src/views/transactions/index.vue
@@ -252,7 +252,7 @@ export default {
       const projectId = item.project?.id || '';
       const categoryIds = item.categories.map(v => v.id);
       return `/transactions/new?account=${accountId}&project=${projectId}` +
-        `&category=${categoryIds}&description=${item.description}` +
+        `&category=${categoryIds}&description=${item.description || ''}` +
         `&amount=${Math.abs(item.amount)}&isIncome=${item.amount > 0}`;
     },
     onAccount(account) {
