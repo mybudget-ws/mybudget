40a9466ce7e5861cccb4adb627fb43a7403568d6
blackchestnut <alexander.kalinichev@gmail.com>
2020-07-25 21:44:54 +0300

    Experiment with cookies store

--- src/utils/cookie_store.js
+++ src/utils/cookie_store.js
@@ -1,8 +1,16 @@
 import cookies from 'browser-cookies';
 
+const DOMAIN = {
+  development: 'localhost',
+  production: 'mybudget.ws'
+}[process.env.NODE_ENV];
+
 export default {
   set(name, key, value) {
-    cookies.set(`${name}_${key}`, JSON.stringify(value), { expires: 365 });
+    cookies.set(
+      `${name}_${key}`,
+      JSON.stringify(value), { expires: 365, domain: DOMAIN, samesite: 'Lax' }
+    );
   },
   get(name, key, defaultValue) {
     const value = cookies.get(`${name}_${key}`);
