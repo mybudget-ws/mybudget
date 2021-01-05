271c6b2fd0846673787677cb33c4c9ecff67c8dc
blackchestnut <alexander.kalinichev@gmail.com>
2021-01-05 14:39:15 +0300

    Remove Sentry.init from non production env

--- src/main.js
+++ src/main.js
@@ -13,10 +13,12 @@ if (process.env.NODE_ENV === 'production') {
 
 import * as Sentry from '@sentry/browser';
 import { Vue as VueIntegration } from '@sentry/integrations';
-Sentry.init({
-  dsn: 'https://31a2997a7521403fa04ac9e939bd5bf2@o242649.ingest.sentry.io/5266881',
-  integrations: [new VueIntegration({ Vue, attachProps: true })]
-});
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://31a2997a7521403fa04ac9e939bd5bf2@o242649.ingest.sentry.io/5266881',
+    integrations: [new VueIntegration({ Vue, attachProps: true })]
+  });
+}
 
 import 'materialize-css';
 import 'materialize-css/dist/css/materialize.css';
