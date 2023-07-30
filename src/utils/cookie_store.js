b64136aff1672ba3b6b915ea57e07a60711e6765
blackchestnut <alexander.kalinichev@gmail.com>
2023-07-30 20:00:55 +0300

    Work around for 7-days cookeis limit

--- src/utils/cookie_store.js
+++ src/utils/cookie_store.js
@@ -13,10 +13,18 @@ export default {
     );
   },
   get(name, key, defaultValue) {
-    const value = cookies.get(`${name}_${key}`);
-
-    return value ?
-      JSON.parse(value) :
-      defaultValue;
+    try {
+      const value = cookies.get(`${name}_${key}`);
+      // Переустанавливаем куки заново, чтобы обойти ограничение в 7 дней
+      // для Safari и Brave.
+      if (value) {
+        const parsedValue = JSON.parse(value);
+        this.set(name, key, parsedValue);
+        return parsedValue;
+      }
+      return defaultValue;
+    } catch  {
+      return defaultValue;
+    }
   }
 };
